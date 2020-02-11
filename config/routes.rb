e823aeb1bb402f435eb92a350cc1180c2255fb69
ask1208 <t.ask.128@icloud.com>
2020-02-11 20:43:44 +0900

    route変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
 
   
-  devise_for :users
+  devise_for :users,controllers:{
+    :confirmations => 'users/confirmations',
+  }
   root "items#index"
+  resources :users, only: [:show, :new] do
+    get :profile_edit
+  end
     
 
   resources :signups, only: [:new, :create] do
     collection do
       get 'user1'
       post 'user2'
-      get 'user3'
+      post 'user3'
       get 'login'
     end
   end
